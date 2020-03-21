e99dfe30a8b18024db4991129e65200a97db704a
Tuwhakatere <61450241+Tuwhakatere@users.noreply.github.com>
2020-03-21 19:10:38 +1100

    Create baseconversion.py

---
+++ baseconversion.py
@@ -0,0 +1,22 @@
+num, base = input('Enter number and base: ').split(', ')
+
+num = int(num)
+base = int(base)
+
+retList = []
+if base <= 16:
+        while True:
+                if not (num > 0):
+                        break
+                else:
+                        rem = num % base
+                        num = num // base
+                        retList.append(rem)
+
+final = []
+i = len(retList) - 1
+while i >= 0:
+        final.append(str(retList[i]))
+        i = i - 1
+
+print(''.join(final)) 
