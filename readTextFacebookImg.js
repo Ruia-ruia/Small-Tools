eb5a3c1da6cd3de230bc8d82a97f44c79449a313
Tuwhakatere <61450241+Tuwhakatere@users.noreply.github.com>
2020-03-23 19:36:42 +1100

    Create readTextFacebookImg.js

---
+++ readTextFacebookImg.js
@@ -0,0 +1,12 @@
+/*
+1. Left-click image. It should now be enlarged on the screen.
+2. Right-click in the browser and select 'inspect element' and then go to the second tab which reads 'Console'.
+3. Alternatively, use 'Command + Option + I' and it should go to the 'Console' tab automatically. 
+   This option is for Mac OSX.
+4. Input the JavaScript code exactly it is appears, into the 'Console' which comes up:
+
+5. Press enter and the text which is in the image should be appended to the 
+end of a few words saying 'Possible text contained in image'.
+*/
+
+document.getElementsByClassName('spotlight')[0].alt
