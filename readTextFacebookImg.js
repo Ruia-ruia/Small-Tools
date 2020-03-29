019c5313b0e856a4a7b2ef3bbc80aae815f74c3d
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-03-29 21:04:32 +1100

    Update readTextFacebookImg.js

--- readTextFacebookImg.js
+++ readTextFacebookImg.js
@@ -2,7 +2,7 @@
 Creating a google chrome extension. 
 
 Will either go with the creation of a dual-facebook which opens in another tab and collects
-the alt part of all img tags on facebook. Otherwise I will change the actual tags to <p>
+the alt part of all img tags on facebook. Otherwise I will change the actual tags to <h3>
 and inline the alt to replace the images altogether. Perhaps these can be options on the 
 extension. 
 */
